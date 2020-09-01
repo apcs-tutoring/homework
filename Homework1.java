1ec39d623588dde85f66edda2aa78f4ef7e491f7
Yitian <yitian.zou@berkeley.edu>
2020-08-31 19:10:41 -0700

    Homework 1 assigned

---
+++ Homework1.java
@@ -0,0 +1,45 @@
+/*
+Homework #1
+Assigned 8/31/20
+*/
+
+public class Homework1 {
+    /* 
+    Q1 - Write a method that returns the length of a string s
+    Don't use .length or .size()
+    stringLength("super") -> 5
+    */
+    public int stringLength(String s) {
+        // TODO: write Q1 code
+        return 0;
+    }
+
+    /*
+    Q2 - Divide integer a by integer b given a specified rounding method
+    "exact" = get exact decimal value
+    "floor" = round down to nearest int
+    "ceiling" = round up to nearest int
+    divideInteger(5, 2, "exact") -> 2.5
+    divideInteger(5, 2, "floor") -> 2.0
+    divideInteger(5, 2, "ceiling") -> 3.0
+    */
+    public double divideInteger(int a, int b, String rounding) {
+        // TODO: write Q2 code
+        return 0.0;
+    }
+
+    /*
+    Q3 - Return true if the input array has an even length, and false if it's odd
+    evenLength([1, 2, 3]) -> false
+    evenLength([1, 2, 3, 4]) -> true
+    */
+    public boolean evenLength(int[] arr) {
+        // TODO: write Q3 code
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+        // TODO: test your code!
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# homework
+# APCS Tutoring Homework
+
+## Process
+* Every week after our 1-on-1 session, I will upload a new homework file.
+You have until the beginning of our next session to do the assignment and upload onto Github.
+* If anything is unclear in the assignment, please contact me via Google Classroom.
+* For any questions you don't know how to do, try to write down in a comment what your thought process is, even if you think you're wrong. We can go over the problem next session.
